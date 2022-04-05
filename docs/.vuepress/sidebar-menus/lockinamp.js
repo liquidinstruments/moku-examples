5db68aecc9670a2026ca60ce1b63750ca44d9d09
Sasikanth Yalamarthy <sashi@liquidinstruments.com>
2022-04-05 13:14:21 +1000

    LIA: Use embedded doc definitons

--- docs/.vuepress/sidebar-menus/lockinamp.js
+++ docs/.vuepress/sidebar-menus/lockinamp.js
@@ -4,8 +4,6 @@ module.exports = {
     children: [
         ["/reference/lia/", "Overview"],
         ["/reference/lia/enable_rollmode", "enable_rollmode"],
-        ["/reference/lia/get_data", "get_data"],
-        ["/reference/lia/logging_progress", "logging_progress"],
         ["/reference/lia/pll_reacquire", "pll_reacquire"],
         ["/reference/lia/set_aux_output", "set_aux_output"],
         ["/reference/lia/set_by_frequency", "set_by_frequency"],
@@ -15,15 +13,29 @@ module.exports = {
         ["/reference/lia/set_frontend", "set_frontend"],
         ["/reference/lia/set_gain", "set_gain"],
         ["/reference/lia/set_hysteresis", "set_hysteresis"],
-        ["/reference/lia/set_monitor", "set_monitor"],
         ["/reference/lia/set_outputs", "set_outputs"],
         ["/reference/lia/set_pll_bandwidth", "set_pll_bandwidth"],
         ["/reference/lia/set_polar_mode", "set_polar_mode"],
-        ["/reference/lia/set_timebase", "set_timebase"],
-        ["/reference/lia/set_trigger", "set_trigger"],
-        ["/reference/lia/start_logging", "start_logging"],
-        ["/reference/lia/stop_logging", "stop_logging"],
         ["/reference/lia/summary", "summary"],
         ["/reference/lia/use_pid", "use_pid"],
+        {
+            title: 'Logger',
+            collapsable: true,
+            children: [
+                ["/reference/embedded/edl/start_logging", "start_logging"],
+                ["/reference/embedded/edl/stop_logging", "stop_logging"],
+                ["/reference/embedded/edl/logging_progress", "logging_progress"]
+            ]
+        },
+        {
+            title: 'Monitors',
+            collapsable: true,
+            children: [
+                ["/reference/embedded/eos/get_data", "get_data"],
+                ["/reference/embedded/eos/set_monitor", "set_monitor"],
+                ["/reference/embedded/eos/set_timebase", "set_timebase"],
+                ["/reference/embedded/eos/set_trigger", "set_trigger"],
+            ]
+        },
     ]
 };
