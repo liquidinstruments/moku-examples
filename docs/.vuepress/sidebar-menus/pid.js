30b133c905b0ea2aca8469e95e138e351326d937
Sasikanth Yalamarthy <sashi@liquidinstruments.com>
2022-04-05 13:12:24 +1000

    PID: Use embedded doc definitions

--- docs/.vuepress/sidebar-menus/pid.js
+++ docs/.vuepress/sidebar-menus/pid.js
@@ -5,7 +5,6 @@ module.exports = {
         ["/reference/pid/", "Overview"],
         ["/reference/pid/enable_input", "enable_input"],
         ["/reference/pid/enable_output", "enable_output"],
-        ["/reference/pid/get_data", "get_data"],
         ["/reference/pid/set_by_frequency", "set_by_frequency"],
         ["/reference/pid/set_by_gain_and_section", "set_by_gain_and_section"],
         ["/reference/pid/set_by_gain", "set_by_gain"],
@@ -14,10 +13,26 @@ module.exports = {
         ["/reference/pid/set_frontend", "set_frontend"],
         ["/reference/pid/set_hysteresis", "set_hysteresis"],
         ["/reference/pid/set_input_offset", "set_input_offset"],
-        ["/reference/pid/set_monitor", "set_monitor"],
         ["/reference/pid/set_output_offset", "set_output_offset"],
-        ["/reference/pid/set_timebase", "set_timebase"],
-        ["/reference/pid/set_trigger", "set_trigger"],
         ["/reference/pid/summary", "summary"],
+        {
+            title: 'Logger',
+            collapsable: true,
+            children: [
+                ["/reference/embedded/edl/start_logging", "start_logging"],
+                ["/reference/embedded/edl/stop_logging", "stop_logging"],
+                ["/reference/embedded/edl/logging_progress", "logging_progress"]
+            ]
+        },
+        {
+            title: 'Monitors',
+            collapsable: true,
+            children: [
+                ["/reference/embedded/eos/get_data", "get_data"],
+                ["/reference/embedded/eos/set_monitor", "set_monitor"],
+                ["/reference/embedded/eos/set_timebase", "set_timebase"],
+                ["/reference/embedded/eos/set_trigger", "set_trigger"],
+            ]
+        },
     ]
 };
