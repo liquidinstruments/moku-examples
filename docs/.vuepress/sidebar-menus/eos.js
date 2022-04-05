e61ccc9e1c709a02df2d18291f4535723957cfda
Sasikanth Yalamarthy <sashi@liquidinstruments.com>
2022-04-05 14:29:05 +1000

    Export EOS and EDL definitions as a module

---
+++ docs/.vuepress/sidebar-menus/eos.js
@@ -0,0 +1,13 @@
+module.exports = {
+    title: 'Monitors',
+    collapsable: true,
+    children: [
+        ["/reference/embedded/eos/enable_rollmode", "enable_rollmode"],
+        ["/reference/embedded/eos/get_data", "get_data"],
+        ["/reference/embedded/eos/save_high_res_buffer", "save_high_res_buffer"],
+        ["/reference/embedded/eos/set_acquisition_mode", "set_acquisition_mode"],
+        ["/reference/embedded/eos/set_hysteresis", "set_hysteresis"],
+        ["/reference/embedded/eos/set_timebase", "set_timebase"],
+        ["/reference/embedded/eos/set_trigger", "set_trigger"],
+    ]
+}
