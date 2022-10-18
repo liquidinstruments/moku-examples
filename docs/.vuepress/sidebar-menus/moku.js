ea6f1f56d8f9453d849c97667bf424f269e05527
Sasikanth Yalamarthi <sashi@liquidinstruments.com>
2022-10-19 09:23:01 +1100

    Add moku/platform docco

--- docs/.vuepress/sidebar-menus/moku.js
+++ docs/.vuepress/sidebar-menus/moku.js
@@ -6,6 +6,7 @@ module.exports = {
         ['/reference/moku/claim_ownership', 'claim_ownership'],
         ['/reference/moku/describe', 'describe'],
         ['/reference/moku/name', 'name'],
+        ['/reference/moku/platform', 'platform'],
         ['/reference/moku/relinquish_ownership', 'relinquish_ownership'],
         ['/reference/moku/serial_number', 'serial_number'],
     ]
