711a8efa9adcb3c474be0bc451a59a851a3e9f3c
nadia <nadia@liquidinstruments.com>
2025-08-27 13:38:03 +1000

    MINOR: side panel for mokucli docs

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -182,6 +182,13 @@ module.exports = {
                     initialOpenGroupIndex: -1,
                     children: [
                         ['/cli/', 'Overview'],
+                    ],
+                },
+                {
+                    title: 'Usage',
+                    collapsable: false,
+                    initialOpenGroupIndex: -1,
+                    children: [
                         ['/cli/instrument', 'instrument'],
                         ['/cli/firmware', 'firmware'],
                         ['/cli/feature', 'feature'],
