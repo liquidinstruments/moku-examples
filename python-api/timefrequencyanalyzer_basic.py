5603bbcfc4aa620f92004d17ce552fc599766a91
indira <indira@liquidinstruments.com>
2025-12-05 19:35:19 +1100

    Add TFA functions set_interval_policy and interval 'type'

--- python-api/timefrequencyanalyzer_basic.py
+++ python-api/timefrequencyanalyzer_basic.py
@@ -27,6 +27,9 @@ try:
     # Set acquisition mode to a 100ms Window
     i.set_acquisition_mode(mode='Windowed', window_length=100e-3)
 
+    # Use the first start event and close the interval at the end of the gate period (arbitrary as using windowed acquisition)
+    i.set_interval_policy(multiple_start_events='Use first', incomplete_intervals='Close')
+
     # Set the interval analyzers
     # Set Interval A to start at Event A and stop at Event A
     # Set Interval B to start at Event B and stop at Event B
